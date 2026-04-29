e87241dbab00a2d0505f5d06654d7b40f5b8d9e4
Jose <alfredo.mori1999@gmail.com>
2026-04-29 17:23:43 -0500

    primer commit

---
+++ src/main/java/com/semana3java/springjwt/SpringjwtApplication.java
@@ -0,0 +1,16 @@
+package com.semana3java.springjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringjwtApplication.class, args);
+        System.out.println("Termino.....");
+
+
+    }
+
+}
